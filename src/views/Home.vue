417a8c4f9536c7411a516c33bea7524f422ca51c
Modhe Mastbaum <msh.mstbm@gmail.com>
2019-05-06 20:50:34 +0300

    Add element

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="ברוכים הבאים למוסך גוקוד שדרות " />
   </div>
 </template>
 
